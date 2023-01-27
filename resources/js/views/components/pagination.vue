c92c74da0c66807378fa92315d7f7f2b9660df56
soulaimane yahya <soulaimaneyh07@gmail.com>
2023-01-27 14:10:03 +0100

    fix - pagination

--- resources/js/views/components/pagination.vue
+++ resources/js/views/components/pagination.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="d-flex justify-content-center gap-1">
     <router-link
-      v-for="(link, index) in links" 
-      :key="index" 
-      class="py-2 px-4 rounded-md" 
-      :class="{'link-active': link.active}"
-      :to="{ name: 'products.index', query: {page: index} }"
+      v-for="(link, index) in links" :key="index" class="py-2 px-4 rounded-md"
+      :class="{'link-active': link.active}" :to="{ name: 'products.index', query: { page: nextPage(link.url) } }"
       v-html="link.label"
     />
   </div>
@@ -16,13 +13,21 @@ export default {
   props: {
     links: Array,
   },
+  methods: {
+    nextPage(url) {
+      const page = url ? url.split('?')[1].split('=')[1] : 1
+      console.log(page)
+      return page
+    },
+  },
 }
 </script>
 
 <style>
 .link-active {
-    color: dark;
-    font-weight: 600;
-    text-decoration: underline;
+  color: dark;
+  font-weight: 600;
+  color: black;
+  text-decoration: underline;
 }
 </style>
